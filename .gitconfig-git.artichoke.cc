0a210d82c9f16ee6c8489de5b0a718dc90526947
artichoked1 <41409702+artichoked1@users.noreply.github.com>
2025-04-27 12:09:36 +0930

    change signing key path

--- .gitconfig-git.artichoke.cc
+++ .gitconfig-git.artichoke.cc
@@ -1,7 +1,7 @@
 [user]
     name = artichoke
     email = git.nikolai@artichoke.cc
-    signingkey = ~/.ssh/id_ed25519_gitea_sign.pub
+    signingkey = ~/.ssh/id_ed25519_gitea_artichokecc_sign.pub
 [gpg]
     format = ssh
 [commit]
